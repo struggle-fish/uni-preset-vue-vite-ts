04700e3675288ca1a068e25e5614c88b9c740281
jingchaoyang <jingchaoyang@autohome.com.cn>
2024-04-28 16:57:37 +0800

    项目初始化

---
+++ src/pages/my/my.vue
@@ -0,0 +1,15 @@
+<template>
+  <view class="my">
+    <text>我的</text>
+    <image
+      src=""
+      mode="aspectFill"
+    />
+  </view>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped></style>
