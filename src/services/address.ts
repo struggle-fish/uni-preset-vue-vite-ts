4844ab22a30b2a6849795494efd35293abe48005
jingchaoyang <jingchaoyang@autohome.com.cn>
2024-05-02 14:07:11 +0800

    地址列表

---
+++ src/services/address.ts
@@ -0,0 +1,39 @@
+import type { AddressItem, AddressParams } from '@/types/address'
+import { http } from '@/utils/http'
+
+export const postMemberAddressAPI = (data: AddressParams) => {
+  return http({
+    method: 'POST',
+    url: '/member/address',
+    data,
+  })
+}
+
+export const getMemberAddressAPI = () => {
+  return http<AddressItem[]>({
+    method: 'GET',
+    url: '/member/address',
+  })
+}
+
+export const getMemberAddressByIdAPI = (id: string | number) => {
+  return http<AddressItem>({
+    method: 'GET',
+    url: `/member/address/${id}`,
+  })
+}
+
+export const putMemberAddressByIdAPI = (id: string | number, data: AddressParams) => {
+  return http({
+    method: 'PUT',
+    url: `/member/address/${id}`,
+    data,
+  })
+}
+
+export const deleteMemberAddressByIdAPI = (id: string) => {
+  return http({
+    method: 'DELETE',
+    url: `/member/address/${id}`,
+  })
+}
