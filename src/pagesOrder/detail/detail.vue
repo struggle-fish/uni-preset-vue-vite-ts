31b0f09de0444986d15b955ea2a662f81cd74111
jingchaoyang <jingchaoyang@autohome.com.cn>
2024-05-05 23:01:51 +0800

    订单详情

--- src/pagesOrder/detail/detail.vue
+++ src/pagesOrder/detail/detail.vue
@@ -2,30 +2,39 @@
   <!-- 自定义导航栏: 默认透明不可见, scroll-view 滚动到 50 时展示 -->
   <view class="navbar" :style="{ paddingTop: safeAreaInsets?.top + 'px' }">
     <view class="wrap">
-      <navigator v-if="true" open-type="navigateBack" class="back icon-left"></navigator>
+      <navigator v-if="pages.length > 1" open-type="navigateBack" class="back icon-left"></navigator>
       <navigator v-else url="/pages/index/index" open-type="switchTab" class="back icon-home">
       </navigator>
       <view class="title">订单详情</view>
     </view>
   </view>
-  <scroll-view scroll-y class="viewport" id="scroller" @scrolltolower="onScrolltolower">
-    <template v-if="true">
+  <scroll-view scroll-y class="viewport" id="scroller" @scrolltolower="onScrolltoLower">
+    <template v-if="order">
       <!-- 订单状态 -->
       <view class="overview" :style="{ paddingTop: safeAreaInsets!.top + 20 + 'px' }">
         <!-- 待付款状态:展示去支付按钮和倒计时 -->
-        <template v-if="true">
+        <template v-if="order?.orderState === OrderState.DaiFuKuan">
           <view class="status icon-clock">等待付款</view>
           <view class="tips">
-            <text class="money">应付金额: ¥ 99.00</text>
+            <view>
+              <text class="money">应付金额: ¥ {{ order.payMoney }}</text>
+            </view>
             <text class="time">支付剩余</text>
-            00 时 29 分 59 秒
+            <uni-countdown
+              :second="order.countdown"
+              color="#fff"
+              splitor-color="#fff"
+              :show-day="false"
+              :show-colon="false"
+              @timeup="onTimeUp"
+            />
           </view>
-          <view class="button">去支付</view>
+          <view class="button" @click="onOrderPay">去支付</view>
         </template>
         <!-- 其他订单状态:展示再次购买按钮 -->
-        <template v-else>
+        <template v-else> 
           <!-- 订单状态文字 -->
-          <view class="status"> 待付款 </view>
+          <view class="status"> {{ orderStateList[order!.orderState].text }} </view>
           <view class="button-group">
             <navigator
               class="button"
@@ -35,23 +44,26 @@
               再次购买
             </navigator>
             <!-- 待发货状态：模拟发货,开发期间使用,用于修改订单状态为已发货 -->
-            <view v-if="false" class="button"> 模拟发货 </view>
+            <view v-if="isDev && order.orderState === OrderState.DaiFaHuo" 
+            @click="onOrderSend"
+            class="button"> 模拟发货 </view>
+            <view v-if="order.orderState === OrderState.DaiShouHuo" @click="onOrderConfirm" class="button"> 确认收货 </view>
           </view>
         </template>
       </view>
       <!-- 配送状态 -->
       <view class="shipment">
         <!-- 订单物流信息 -->
-        <view v-for="item in 1" :key="item" class="item">
+        <view v-for="item in logisticList" :key="item.id" class="item">
           <view class="message">
-            您已在广州市天河区黑马程序员完成取件，感谢使用菜鸟驿站，期待再次为您服务。
+            {{ item.text }}
           </view>
-          <view class="date"> 2023-04-14 13:14:20 </view>
+          <view class="date"> {{ item.time }} </view>
         </view>
         <!-- 用户收货地址 -->
         <view class="locate">
-          <view class="user"> 张三 13333333333 </view>
-          <view class="address"> 广东省 广州市 天河区 黑马程序员 </view>
+          <view class="user"> {{ order.receiverContact }} {{ order.receiverMobile }} </view>
+          <view class="address"> {{ order.receiverAddress }} </view>
         </view>
       </view>
 
@@ -60,29 +72,29 @@
         <view class="item">
           <navigator
             class="navigator"
-            v-for="item in 2"
-            :key="item"
-            :url="`/pages/goods/goods?id=${item}`"
+            v-for="item in order.skus"
+            :key="item.id"
+            :url="`/pages/goods/goods?id=${item.spuId}`"
             hover-class="none"
           >
             <image
               class="cover"
-              src="https://yanxuan-item.nosdn.127.net/c07edde1047fa1bd0b795bed136c2bb2.jpg"
+              :src="item.image"
             ></image>
             <view class="meta">
-              <view class="name ellipsis">ins风小碎花泡泡袖衬110-160cm</view>
-              <view class="type">藏青小花， 130</view>
+              <view class="name ellipsis">{{ item.name }}</view>
+              <view class="type">{{ item.attrsText }}</view>
               <view class="price">
                 <view class="actual">
                   <text class="symbol">¥</text>
-                  <text>99.00</text>
+                  <text>{{ item.curPrice }}</text>
                 </view>
               </view>
-              <view class="quantity">x1</view>
+              <view class="quantity">x{{ item.quantity }}</view>
             </view>
           </navigator>
           <!-- 待评价状态:展示按钮 -->
-          <view class="action" v-if="true">
+          <view class="action" v-if="order.orderState === OrderState.DaiPingJia">
             <view class="button primary">申请售后</view>
             <navigator url="" class="button"> 去评价 </navigator>
           </view>
@@ -91,15 +103,15 @@
         <view class="total">
           <view class="row">
             <view class="text">商品总价: </view>
-            <view class="symbol">99.00</view>
+            <view class="symbol">{{ order.totalMoney }}</view>
           </view>
           <view class="row">
             <view class="text">运费: </view>
-            <view class="symbol">10.00</view>
+            <view class="symbol">{{ order.postFee }}</view>
           </view>
           <view class="row">
             <view class="text">应付金额: </view>
-            <view class="symbol primary">109.00</view>
+            <view class="symbol primary">{{ order.payMoney }}</view>
           </view>
         </view>
       </view>
@@ -111,7 +123,7 @@
           <view class="item">
             订单编号: {{ query.id }} <text class="copy" @tap="onCopy(query.id)">复制</text>
           </view>
-          <view class="item">下单时间: 2023-04-14 13:14:20</view>
+          <view class="item">下单时间: {{ order.createTime }}</view>
         </view>
       </view>
 
@@ -122,7 +134,7 @@
       <view class="toolbar-height" :style="{ paddingBottom: safeAreaInsets?.bottom + 'px' }"></view>
       <view class="toolbar" :style="{ paddingBottom: safeAreaInsets?.bottom + 'px' }">
         <!-- 待付款状态:展示支付按钮 -->
-        <template v-if="true">
+        <template v-if="order.orderState === OrderState.DaiFuKuan">
           <view class="button primary"> 去支付 </view>
           <view class="button" @tap="popup?.open?.()"> 取消订单 </view>
         </template>
@@ -136,11 +148,11 @@
             再次购买
           </navigator>
           <!-- 待收货状态: 展示确认收货 -->
-          <view class="button primary"> 确认收货 </view>
+          <view class="button primary" v-if="order.orderState === OrderState.DaiShouHuo"> 确认收货 </view>
           <!-- 待评价状态: 展示去评价 -->
-          <view class="button"> 去评价 </view>
+          <view class="button" v-if="order.orderState === OrderState.DaiPingJia"> 去评价 </view>
           <!-- 待评价/已完成/已取消 状态: 展示删除订单 -->
-          <view class="button delete"> 删除订单 </view>
+          <view class="button delete" @click="onOrderDelete" v-if="order.orderState >= OrderState.DaiShouHuo"> 删除订单 </view>
         </template>
       </view>
     </template>
@@ -168,35 +180,131 @@
   </uni-popup>
 </template>
 <script setup lang="ts">
-import { useGuessList } from '@/composables'
-import { ref } from 'vue'
+  import { useGuessList } from '@/composables'
+  import { deleteMemberOrderAPI, getMemberOrderByIdAPI, getMemberOrderConsignmentByIdAPI, getMemberOrderLogisticsByIdAPI, putMemberOrderReceiptByIdAPI } from '@/services/oreder';
+  import type { OrderResult, LogisticItem } from '@/types/order';
+  import { onReady, onShow } from '@dcloudio/uni-app';
+  import { ref } from 'vue'
+  import { OrderState } from '@/services/constants';
+  import { orderStateList } from '../../services/constants';
+  import { getPayWxPayMiniPayAPI, getPayMockAPI } from '@/services/pay';
+  // 获取屏幕边界到安全区域距离
+  const { safeAreaInsets } = uni.getSystemInfoSync()
+  // 猜你喜欢
+  const { guessRef, onScrolltoLower } = useGuessList()
+  // 弹出层组件
+  const popup = ref<UniHelper.UniPopupInstance>()
+  // 取消原因列表
+  const reasonList = ref([
+    '商品无货',
+    '不想要了',
+    '商品信息填错了',
+    '地址信息填写错误',
+    '商品降价',
+    '其它',
+  ])
+  // 订单取消原因
+  const reason = ref('')
+  // 复制内容
+  const onCopy = (id: string) => {
+    // 设置系统剪贴板的内容
+    uni.setClipboardData({ data: id })
+  }
+  // 获取页面参数
+  const query = defineProps<{
+    id: string
+  }>()
 
-// 获取屏幕边界到安全区域距离
-const { safeAreaInsets } = uni.getSystemInfoSync()
-// 猜你喜欢
-const { guessRef, onScrolltoLower } = useGuessList()
-// 弹出层组件
-const popup = ref<UniHelper.UniPopupInstance>()
-// 取消原因列表
-const reasonList = ref([
-  '商品无货',
-  '不想要了',
-  '商品信息填错了',
-  '地址信息填写错误',
-  '商品降价',
-  '其它',
-])
-// 订单取消原因
-const reason = ref('')
-// 复制内容
-const onCopy = (id: string) => {
-  // 设置系统剪贴板的内容
-  uni.setClipboardData({ data: id })
-}
-// 获取页面参数
-const query = defineProps<{
-  id: string
-}>()
+  const pages = getCurrentPages()
+  // 获取页面当前实例
+  const pageInstance = pages.at(-1) as any
+  onReady(() => {
+    pageInstance.animate('.navbar',
+      [{ backgroundColor: 'transparent' }, { backgroundColor: '#f8f8f8' }],
+      1000,
+      {
+        scrollSource: '#scroller',
+        timeRange: 1000,
+        startScrollOffset: 0,
+        endScrollOffset: 50,
+      },
+    )
+  })
+
+  const order = ref<OrderResult>()
+  const getMemberOrderByIdData = async() => {
+    const res = await getMemberOrderByIdAPI(query.id)
+    order.value = res.result
+    if ([OrderState.DaiShouHuo, OrderState.DaiPingJia, OrderState.YiWanCheng].includes(order.value.orderState)) {
+      getMemberOrderLogisticsByIdData()
+    }
+  }
+
+  const onTimeUp = () => {
+    order.value!.orderState = OrderState.YiQuXiao
+  }
+
+
+  const onOrderPay = async() => {
+    if (import.meta.env.DEV) {
+      await getPayMockAPI({ orderId: query.id })
+    } else {
+      const res = await getPayWxPayMiniPayAPI({orderId: query.id})
+      wx.requestPayment(res.result)
+    }
+    // // 关闭当前页面在跳转
+    // uni.redirectTo({
+    //   url: `/pagesOrder/payment/payment?id=${query.id}`
+    // })
+  }
+
+
+  const isDev = import.meta.env.DEV
+  const onOrderSend = async () => {
+    if (isDev) {
+      await getMemberOrderConsignmentByIdAPI(query.id)
+      uni.showToast({
+        icon: 'none',
+        title: '模拟发货完成'
+      })
+      order.value!.orderState = OrderState.DaiShouHuo
+    }
+  }
+
+
+  const onOrderConfirm = async() => {
+    uni.showModal({
+      content: '为保障您的权益，请收到货后确认无误，在确认收货',
+      success: async(res)=>{
+        const resp = await putMemberOrderReceiptByIdAPI(query.id)
+        order.value = resp.result
+      },
+    })
+  }
+
+  const logisticList = ref<LogisticItem[]>([])
+  const getMemberOrderLogisticsByIdData = async() => {
+    const res = await getMemberOrderLogisticsByIdAPI(query.id)
+    logisticList.value = res.result.list
+  }
+
+  const onOrderDelete = async () => {
+    uni.showModal({
+      content:'是否删除订单',
+      success: async(res)=>{
+        if (res.confirm) {
+          await deleteMemberOrderAPI({
+            ids: [query.id]
+          })
+          uni.redirectTo({ url: '/pagesOrder/list/list' })
+        }
+      },
+    })
+  }
+
+  onShow(() => {
+    getMemberOrderByIdData()
+  })
 </script>
 <style lang="scss">
 page {
---
+++ src/services/pay.ts
@@ -0,0 +1,17 @@
+import { http } from '@/utils/http'
+
+export const getPayWxPayMiniPayAPI = (data: { orderId: string }) => {
+  return http<WechatMiniprogram.RequestPaymentOption>({
+    method: 'GET',
+    url: '/pay/wxPay/miniPay',
+    data,
+  })
+}
+
+export const getPayMockAPI = (data: { orderId: string }) => {
+  return http({
+    method: 'GET',
+    url: '/pay/mock',
+    data,
+  })
+}
