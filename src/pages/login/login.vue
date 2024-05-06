ed88ba17b9e4faca1371931aa26d74436995b793
jingchaoyang <jingchaoyang@autohome.com.cn>
2024-05-06 08:00:18 +0800

    添加登录区分

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -7,15 +7,19 @@
     </view>
     <view class="login">
       <!-- 网页端表单登录 -->
-      <!-- <input class="input" type="text" placeholder="请输入用户名/手机号码" /> -->
-      <!-- <input class="input" type="text" password placeholder="请输入密码" /> -->
-      <!-- <button class="button phone">登录</button> -->
+      <!-- #ifdef H5 -->
+      <input class="input" type="text" placeholder="请输入用户名/手机号码" />
+      <input class="input" type="text" password placeholder="请输入密码" />
+      <button class="button phone">登录</button>
+      <!-- #endif -->
 
       <!-- 小程序端授权登录 -->
+      <!-- #ifdef MP-WEIXIN -->
       <button class="button phone" open-type="getPhoneNumber" @getphonenumber="onGetPhoneNumber">
         <text class="icon icon-phone"></text>
         手机号快捷登录
       </button>
+      <!-- #endif -->
       <view class="extra">
         <view class="caption">
           <text>其他登录方式</text>
@@ -37,6 +41,7 @@ import { useMemberStore } from '@/stores';
 import type { LoginResult } from '@/types/member';
 import { onLoad } from '@dcloudio/uni-app';
 
+  // #ifdef MP-WEIXIN
   let code = ''
   // 获取 code 登录凭证
   onLoad(async () => {
@@ -55,6 +60,7 @@ import { onLoad } from '@dcloudio/uni-app';
     })
     loginSuccess(res.result)
   }
+  // #endif
 
   // 模式手机号快捷登录
   const onGetPhoneNumberSimple = async() => {
